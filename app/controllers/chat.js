f4c1412187fe8718a0f7b4bda832cbba58e9c0f0
Mateus I <mateuspi.0398@gmail.com>
2022-04-18 11:27:20 -0300

    Validação dos dados
    Implementação do código para validar o "nome" que será passado para o chat

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,13 @@
+const { validationResult} = require('express-validator')
 module.exports.iniciarChat = function(application,req,res){
+    const dadosForm = req.body
+
+    var erros = validationResult(req)
+    
+    if(!erros.isEmpty()){
+        res.render('index', {validacao: erros})
+        return
+    }
+
     res.render('chat')
 }
--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,4 +1,11 @@
+const {check} = require('express-validator')
 module.exports = function (application) {
+    application.post('/chat',[
+        check('apelido', 'Nome é obrigatório!').notEmpty(),
+        check('apelido', 'Nome tem que ter entre 3 e 15 caracteres').isLength({ min: 3, max: 15 })
+    ], function (req, res) {
+        application.app.controllers.chat.iniciarChat(application, req, res)
+    })
     application.get('/chat', function (req, res) {
         application.app.controllers.chat.iniciarChat(application, req, res)
     })
